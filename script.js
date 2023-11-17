20b2c46ec5a90c6af514548e4023e6955b25e01a
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-17 10:49:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+	const cleanedHex=shortHex.replace(/^#/,'');
+	let result='';
+	for(let i=0;i<cleanedHex.length;i++)
+		{
+			result+=cleanedHex[i]+cleanedHex[i];
+		}
+	return `#${result}`
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+ alert(extendHex(shortHex));
